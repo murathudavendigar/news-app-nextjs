676c6649b11ea253964e2d9455a6bad77ec62bc4
Captain Price <murathoncu@gmail.com>
2023-01-27 03:38:25 +0300

    add article comp

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 
 //! COMPONENTS
 import Header from "../components/Header";
+import Providers from "./Providers";
 
 export default function RootLayout({
   children,
@@ -11,13 +12,13 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head />
-
-      <body className="bg-gray-100  transition-all duration-700">
-        {/* dark:bg-zinc-900 eklenecek. sistem default olarak tanıyor ekleyemedim */}
-        <Header />
-        <div className="max-w-6xl mx-auto">{children}</div>
-      </body>
+      <Providers>
+        <body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
+          {/* dark:bg-zinc-900 eklenecek. sistem default olarak tanıyor ekleyemedim */}
+          <Header />
+          <div className="max-w-6xl mx-auto">{children}</div>
+        </body>
+      </Providers>
     </html>
   );
 }
