44644ccd90e7b8cfbd1f7f357a9a49b85ae35e34
Captain Price <murathoncu@gmail.com>
2023-01-27 04:35:57 +0300

    add spec category page

---
+++ app/news/[category]/page.tsx
@@ -0,0 +1,28 @@
+import NewsList from "../../../components/NewsList";
+import { categories } from "../../../constants";
+import fetchNews from "../../../lib/fetchNews";
+
+type Props = {
+  params: { category: Category };
+};
+
+const NewsCategoryPage = async ({ params: { category } }: Props) => {
+  const news: NewsResponse = await fetchNews(category);
+
+  return (
+    <div>
+      <h1 className="headerTitle">{category}</h1>
+      <NewsList news={news} />
+    </div>
+  );
+};
+
+export default NewsCategoryPage;
+
+export async function generateStaticParams() {
+  return categories.map((category) => ({
+    category: category,
+  }));
+}
+
+//
