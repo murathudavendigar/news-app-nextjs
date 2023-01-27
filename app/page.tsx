3e4134a0c0d5c0ba19ca427745a3d183c47185f6
Captain Price <murathoncu@gmail.com>
2023-01-27 03:41:06 +0300

    add providers and removed head tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
+import NewsList from "../components/NewsList";
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
+import response from "../response.json";
 
 const Homepage = async () => {
-  const news: NewsResponse = await fetchNews(categories.join(","));
+  const news: NewsResponse =
+    response || (await fetchNews(categories.join(",")));
 
-  console.log(news);
-
-  return <div></div>;
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
 };
 
 export default Homepage;
