2bb2deb509445f1bff5cbb06205db80cb6ce5228
Captain Price <murathoncu@gmail.com>
2023-01-27 03:39:47 +0300

    add dark mode button

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import SearchBox from "./SearchBox";
 
 //! 3rd PARTY LIB
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import DarkModeButton from "./DarkModeButton";
 
 const Header = () => {
   return (
@@ -24,6 +25,7 @@ const Header = () => {
         </Link>
 
         <div className="flex items-center justify-end space-x-2">
+          <DarkModeButton />
           <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
             Subscribe
           </button>
