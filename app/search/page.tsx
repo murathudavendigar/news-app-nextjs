2cb0461a4ffe520f693830ac1a1b8966bfb7ca05
Captain Price <murathoncu@gmail.com>
2023-01-27 04:36:12 +0300

    add search result page

---
+++ app/search/page.tsx
@@ -0,0 +1,23 @@
+import NewsList from "../../components/NewsList";
+import fetchNews from "../../lib/fetchNews";
+
+type Props = {
+  searchParams?: { term: string };
+};
+
+const SearchPage = async ({ searchParams }: Props) => {
+  const news: NewsResponse = await fetchNews(
+    "general",
+    searchParams?.term,
+    true
+  );
+
+  return (
+    <div>
+      <h1 className="headerTitle">Search Reslts for: {searchParams?.term}</h1>
+      <NewsList news={news} />
+    </div>
+  );
+};
+
+export default SearchPage;
